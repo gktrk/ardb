2610b1d8e81aa71ab7794be6884fa38c1e795937
graham.r.smith <graham.r.smith@945c3bdd-e53d-0410-aa76-77bfce44df11>
2007-11-04 09:18:45 +0000

    Initial version of a panel that displays an image. Has issues when panel is resized.

---
+++ trunk/wxARDB/src/imagePanel.h
@@ -0,0 +1,41 @@
+#ifndef IMAGEPANEL_H
+#define IMAGEPANEL_H
+
+#include <wx/wx.h>
+#include <wx/event.h>
+#include <wx/image.h>
+#include <wx/panel.h>
+
+/** The main program frame
+ * 
+ *  This shows the main window for the program which includes the menu, a
+ *  status bar and a panel in which to show the relevant image.
+ */
+
+class ImagePanel : public wxPanel 
+{
+public:
+    /** \name Constructors and Destructor
+     *@{
+     */
+    ImagePanel(wxWindow *parent); ///< Default constructor
+
+    ~ImagePanel(); ///< Destructor
+
+    //@}
+
+    void SetImage(wxString fileName); ///< Set the current active image
+	void Clear(); ///< Set the current active image
+
+protected:
+    void OnPaint(wxPaintEvent &event); ///< Handle paint events for the panel
+
+private:
+    wxImage image;
+
+protected:
+    DECLARE_EVENT_TABLE()
+};
+
+#endif
+
