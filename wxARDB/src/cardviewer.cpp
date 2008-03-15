20746d7fe36f9056797e160821e8272aa2da0646
graham.r.smith <graham.r.smith@945c3bdd-e53d-0410-aa76-77bfce44df11>
2008-03-15 20:13:37 +0000

    Added file header

--- wxARDB/src/cardviewer.cpp
+++ wxARDB/src/cardviewer.cpp
@@ -1,9 +1,25 @@
-///////////////////////////////////////////////////////////////////////////
-// C++ code generated with wxFormBuilder (version Feb 20 2008)
-// http://www.wxformbuilder.org/
-//
-// PLEASE DO "NOT" EDIT THIS FILE!
-///////////////////////////////////////////////////////////////////////////
+/*  Anarch Revolt Deck Builder - a VTES inventory manager / deck builder
+ *
+ *  Copyright (C) 2008 Graham Smith
+ *  graham.r.smith@gmail.com
+ *  
+ *  Official project page: http://code.google.com/p/ardb/
+ *
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.  
+ */
 
 #include "cardviewer.h"
 
