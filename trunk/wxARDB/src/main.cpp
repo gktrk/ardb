383a2fe7cc9c14045c9ddab0156ac2b941753ae4
graham.r.smith <graham.r.smith@945c3bdd-e53d-0410-aa76-77bfce44df11>
2009-06-15 17:49:46 +0000

    Bumped version

--- trunk/wxARDB/src/main.cpp
+++ trunk/wxARDB/src/main.cpp
@@ -416,7 +416,7 @@ void
 BrowserFrame::OnHelpAbout (wxCommandEvent& WXUNUSED (event)) 
 {
 	wxString about;
-	about.Printf(wxT ("The Anarch Revolt Deck Builder\nVersion 2.9.0a\r\nby Francois Gombault & Graham Smith\r\nEmail: graham.r.smith@gmail.com\r\nBuilt: %s"),BUILD_DATE);
+	about.Printf(wxT ("The Anarch Revolt Deck Builder\nVersion 2.9.1\r\nby Francois Gombault & Graham Smith\r\nEmail: graham.r.smith@gmail.com\r\nBuilt: %s"),BUILD_DATE);
 	wxMessageBox(about, wxT ("About"));
 }
 
