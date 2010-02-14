770d1c7cb70ce15bacefda2440fa972c670c18f7
graham.r.smith@gmail.com <graham.r.smith@gmail.com@945c3bdd-e53d-0410-aa76-77bfce44df11>
2010-02-14 11:40:45 +0000

    Creates a base distro for windows

---
+++ trunk/wxARDB/create_msys_dist.sh
@@ -0,0 +1,14 @@
+if [ -d ardb_dist ]; then
+rm -rf ardb_dist
+fi
+mkdir ardb_dist
+strip src/ardb.exe
+cp src/ardb.exe ardb_dist
+mkdir ardb_dist/cardimages
+mkdir ardb_dist/vtesicons
+mkdir ardb_dist/xsl
+cp resources/*.xsl ardb_dist/xsl
+cp resources/vtesicons/*.xpm ardb_dist/vtesicons
+cp resources/ardb.ini ardb_dist
+
+
